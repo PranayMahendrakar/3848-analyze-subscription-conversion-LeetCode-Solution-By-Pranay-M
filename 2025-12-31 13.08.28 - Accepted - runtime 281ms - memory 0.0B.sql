d120d83b0481b5f6eb78a16fbf0c700ee773745f
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-21 12:43:19 +0530

    Initial commit

---
+++ 2025-12-31 13.08.28 - Accepted - runtime 281ms - memory 0.0B.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    t.user_id,
+        ROUND(AVG(t.activity_duration), 2) AS trial_avg_duration,
+            ROUND(AVG(p.activity_duration), 2) AS paid_avg_duration
+            FROM UserActivity t
+            JOIN UserActivity p ON t.user_id = p.user_id
+            WHERE t.activity_type = 'free_trial'
+                AND p.activity_type = 'paid'
+                GROUP BY t.user_id
+                ORDER BY t.user_id;
